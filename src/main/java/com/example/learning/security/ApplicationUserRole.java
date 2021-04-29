2ce23992ed90ba919e9c3d98386fc5f68cce28f7
brulure123 <benraj.200@gmail.com>
2021-04-29 15:57:57 +0200

    Configuration de la page par défaut à afficher après une authentification effectuer avec succès.

--- src/main/java/com/example/learning/security/ApplicationUserRole.java
+++ src/main/java/com/example/learning/security/ApplicationUserRole.java
@@ -1,11 +1,10 @@
 package com.example.learning.security;
 
+import com.google.common.collect.Sets;
 import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.example.learning.security.ApplicationUserPermission.*;
@@ -14,20 +13,20 @@ import static com.example.learning.security.ApplicationUserPermission.*;
 public enum ApplicationUserRole {
 
     // TODO: Replace Collections List with Sets for avoiding duplication
-    STUDENT(Collections.emptyList()),
-    ADMIN(Arrays.asList(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
-    ADMIN_TRAINEE(Arrays.asList(COURSE_READ, STUDENT_READ));
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMIN_TRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
-    private final List<ApplicationUserPermission> permissions;
+    private final Set<ApplicationUserPermission> permissions;
 
-    ApplicationUserRole(List<ApplicationUserPermission> permissions) {
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
     }
 
-    public List<SimpleGrantedAuthority> getGrantedAuthorities() {
-        List<SimpleGrantedAuthority> permissions = getPermissions().stream()
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
                 .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
         permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
