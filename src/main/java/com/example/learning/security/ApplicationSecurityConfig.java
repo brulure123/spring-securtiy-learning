549c6d322495c71447d59f8a9be480bc9bf816cb
brulure123 <benraj.200@gmail.com>
2021-05-01 04:19:14 +0200

    Configuration de la déconnexion avec Spring Security

--- src/main/java/com/example/learning/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/learning/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -51,7 +52,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .rememberMe() // By default the remember me is for two weeks
                     .tokenValiditySeconds((int)TimeUnit.SECONDS.toSeconds(21))
-                    .key("somethingverysecured"); // By default the key is the md5 of username and duration of the remember me.
+                    .key("somethingverysecured") // By default the key is the md5 of username and duration of the remember me.
+                    .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+                    
     }
 
     @Override
